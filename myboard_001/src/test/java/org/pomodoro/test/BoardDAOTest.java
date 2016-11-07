f49d04a81d8f8daf57ba4accea15c81666799e5e
pomodoro <jamesj4318@gmail.com>
2016-11-07 10:42:59 +0900

    'BoardDAOTest.java' is modified on local
    add and modify codes for Ascending test

--- myboard_001/src/test/java/org/pomodoro/test/BoardDAOTest.java
+++ myboard_001/src/test/java/org/pomodoro/test/BoardDAOTest.java
@@ -2,8 +2,10 @@ package org.pomodoro.test;
 
 import javax.inject.Inject;
 
+import org.junit.FixMethodOrder;	// for Ascending test
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,6 +16,7 @@ import org.pomodoro.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)	// for Ascending test
 public class BoardDAOTest {
 	
 	@Inject
@@ -22,34 +25,41 @@ public class BoardDAOTest {
 	private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
 	
 	@Test
-	public void testCreate() throws Exception {
+	public void testA_Create() throws Exception {
 		
-		BoardVO board = new BoardVO();
-		board.setTitle("새로운 글을 넣습니다. ");
-		board.setContent("새로운 글을 넣습니다. ");
-		board.setWriter("user00");
-		dao.create(board);
+		BoardVO board1 = new BoardVO();
+		board1.setTitle("새로운 글 001");
+		board1.setContent("첫번째 글입니다.");
+		board1.setWriter("user_001");
+		dao.create(board1);
+		
+		BoardVO board2 = new BoardVO();
+		board2.setTitle("새로운 글 002");
+		board2.setContent("두번째 글입니다.");
+		board2.setWriter("user_002");
+		dao.create(board2);
 	}
 	
 	@Test
-	public void testRead() throws Exception {
+	public void testB_Read() throws Exception {
 		
-		logger.info(dao.read(7).toString());
+		logger.info(dao.read(1).toString());
+		logger.info(dao.read(2).toString());
 	}
 	
 	@Test
-	public void testUpdate() throws Exception {
+	public void testC_Update() throws Exception {
 		
 		BoardVO board = new BoardVO();
-		board.setBno(6);
-		board.setTitle("수정된 글입니다." );
-		board.setContent("수정 테스트 ");
+		board.setBno(1);
+		board.setTitle("새로운 글 001을 수정했습니다." );
+		board.setContent("첫번째 글을 수정했습니다.");
 		dao.update(board);
 	}
 	
 	@Test
-	public void testDelete() throws Exception {
+	public void testD_Delete() throws Exception {
 		
-		dao.delete(5);
+		dao.delete(2);
 	}
 }
